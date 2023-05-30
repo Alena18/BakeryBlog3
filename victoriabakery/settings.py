126b79dd7ccdaff2872b353794525b04b1d9e8a0
Alena18 <>
2023-05-30 19:32:15 +0000

    Prepare to deploy

--- victoriabakery/settings.py
+++ victoriabakery/settings.py
@@ -27,11 +27,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = os.environ.get("DEBUG", False)
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['localhost',
-   '8000-alena18-bakeryblog3-yn4r443gr8.us2.codeanyapp.com',
+   '8000-alena18-bakeryblog3-yn4r443gr8.us2.codeanyapp.com','victoria-bakery.herokuapp.com/',
 ]
 
 
