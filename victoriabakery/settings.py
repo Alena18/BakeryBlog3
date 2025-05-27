965e85868ac1b395ce80b8e3dbe6ac04858affd3
p250144 <alena.baklanksaya.2025@mumail.ie>
2025-05-27 17:50:08 +0100

    Hopefully last change

--- victoriabakery/settings.py
+++ victoriabakery/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 DEBUG = os.environ.get("DEBUG", True)
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bakeryblog3.onrender.com',
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost', 'bakeryblog3.onrender.com',
                   ]
 
 
