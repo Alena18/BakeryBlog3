f86147208e137a097ff67129586f1b6aaa2f6029
p250144 <alena.baklanksaya.2025@mumail.ie>
2025-05-27 18:51:38 +0100

    Add collectstatic files

--- victoriabakery/settings.py
+++ victoriabakery/settings.py
@@ -163,6 +163,8 @@ else:
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'image')]
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
